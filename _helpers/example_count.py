475081f004d0b83e41efefc2d89940f5684a1b84
Chris Rahmé <chris.rahme@hotmail.com>
2021-03-22 15:27:21 +0200

    Update example_count.py

--- _helpers/example_count.py
+++ _helpers/example_count.py
@@ -1,20 +1,53 @@
 import os
 
-number = 0
+def count(base_dir = '..', starts = '', notstarts = '[IGNORELINE]'):
+    number = 0
 
-base_dir = '../data/nlu'
-dirs = os.listdir(base_dir)
-dirs.remove('nlu.yml')
+    ignore_list = ['nlu.yml']
+    dirs = os.listdir(base_dir)
 
-for d in dirs:
-    local = 0
-    with open('../data/nlu/' + d, 'r', encoding='utf-8') as f:
-        for line in f:
-            l = line.strip()
-            if l.startswith('- ') and not l.startswith('- intent:'):
-                local += 1
-    number += local
-    print(f'{local}\texamples in {d}')
+    for f in ignore_list:
+        if f in dirs:
+            dirs.remove(f)
 
-input(f'\n{number} total NLU examples'
-      f'\n20% = {int(number*0.2)}')
+    for d in dirs:
+        local = 0
+        with open(base_dir + '/' + d, 'r', encoding='utf-8') as f:
+            for line in f:
+                l = line.strip()
+                if l.startswith(starts) and not l.startswith(notstarts):
+                    local += 1
+        number += local
+        #print(f'{local}\t in {d}')
+
+    return number
+
+####################################################################
+
+number = count('../data/nlu', '- intent:')
+
+print(f'\n{number} total intents'
+      f'\n20% = {int(number*0.2)}\n')
+
+####################################################################
+
+number = count('../data/nlu', '- ', '- intent:')
+
+print(f'\n{number} total NLU examples'
+      f'\n20% = {int(number*0.2)}\n')
+
+####################################################################
+
+number = count('../data/stories', '- story:')
+
+print(f'\n{number} total stories'
+      f'\n20% = {int(number*0.2)}\n')
+
+####################################################################
+
+number = count('../data/rules', '- rule:')
+
+print(f'\n{number} total rules'
+      f'\n20% = {int(number*0.2)}\n')
+
+input()
